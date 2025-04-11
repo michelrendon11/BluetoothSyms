855e7a05120c3525bbe4af7b5d0b89b188a62f34
michelrendon11 <michel.rendon11@yahoo.com>
2025-04-10 23:02:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 let server = null;
 let device = null;
-// const disconnectBluetooth = document.getElementById("disconnectBluetooth");
-// const disconnectBluetoothDisplay = disconnectBluetooth.style.display;
+const disconnectBluetoothId = document.getElementById("disconnectBluetoothId");
+const disconnectBluetoothDisplay = disconnectBluetoothId.style.display;
 
 let port = null;
 let reader = null;
@@ -34,6 +34,80 @@ const sym23qrId = document.getElementById("sym23qrId");
 const uSymId = document.getElementById("uSymId");
 const rebootId = document.getElementById("rebootId");
 
+function hideMainButtons(){
+    seeEverythingId.disabled = true;
+    seeEverythingId.classList.add("disable");
+    symBluetoothId.disabled = true;
+    symBluetoothId.classList.add("disable");
+    connectSymId.disabled = true;
+    connectSymId.classList.add("disable");
+}
+
+function showMainButtons(){
+    seeEverythingId.disabled = false;
+    seeEverythingId.classList.remove("disable");
+    symBluetoothId.disabled = false;
+    symBluetoothId.classList.remove("disable");
+    connectSymId.disabled = false;
+    connectSymId.classList.remove("disable");
+}
+
+function hideAllButtons(){
+    scId.disabled = true;
+    spId.disabled = true;
+    sdId.disabled = true;
+    yId.disabled = true;
+    nId.disabled = true;
+    zsSymId.disabled = true;
+    zmSymId.disabled = true;
+    zrSymId.disabled = true;
+    sym23qsId.disabled = true;
+    sym23qrId.disabled = true;
+    uSymId.disabled = true;
+    rebootId.disabled = true;
+
+    scId.classList.add("disable");
+    spId.classList.add("disable");
+    sdId.classList.add("disable");
+    yId.classList.add("disable");
+    nId.classList.add("disable");
+    zsSymId.classList.add("disable");
+    zmSymId.classList.add("disable");
+    zrSymId.classList.add("disable");
+    sym23qsId.classList.add("disable");
+    sym23qrId.classList.add("disable");
+    uSymId.classList.add("disable");
+    rebootId.classList.add("disable");
+}
+
+function showAllButtons(){
+    scId.disabled = false;
+    spId.disabled = false;
+    sdId.disabled = false;
+    yId.disabled = false;
+    nId.disabled = false;
+    zsSymId.disabled = false;
+    zmSymId.disabled = false;
+    zrSymId.disabled = false;
+    sym23qsId.disabled = false;
+    sym23qrId.disabled = false;
+    uSymId.disabled = false;
+    rebootId.disabled = false;
+
+    scId.classList.remove("disable");
+    spId.classList.remove("disable");
+    sdId.classList.remove("disable");
+    yId.classList.remove("disable");
+    nId.classList.remove("disable");
+    zsSymId.classList.remove("disable");
+    zmSymId.classList.remove("disable");
+    zrSymId.classList.remove("disable");
+    sym23qsId.classList.remove("disable");
+    sym23qrId.classList.remove("disable");
+    uSymId.classList.remove("disable");
+    rebootId.classList.remove("disable");
+}
+
 
 window.onload = function(){
     console.log(navigator.platform);
@@ -49,6 +123,8 @@ window.onload = function(){
     }
     platformInfo += "<br/>" + navigator.appVersion;
     document.getElementById("deviceInfo").innerHTML = platformInfo;
+    document.getElementById("displayInfo").innerHTML = "";
+    disconnectBluetoothId.style.display = "none";
 }
 
 async function connectSym(){ 
@@ -71,12 +147,7 @@ async function connectSym(){
         textAreaId.innerHTML += "Connected..." + '\n';
         hhId.disabled = false;
         hhId.classList.remove("disable");
-        seeEverythingId.disabled = true;
-        seeEverythingId.classList.add("disable");
-        symBluetoothId.disabled = true;
-        symBluetoothId.classList.add("disable");
-        connectSymId.disabled = true;
-        connectSymId.classList.add("disable");
+        hideMainButtons();
     }else{
         textAreaId.innerHTML += "Opss... Try againg..." + '\n';
     }
@@ -115,31 +186,7 @@ async function hh() {
     }catch(error){
         console.log(error);
     }
-    scId.disabled = false;
-    spId.disabled = false;
-    sdId.disabled = false;
-    yId.disabled = false;
-    nId.disabled = false;
-    zsSymId.disabled = false;
-    zmSymId.disabled = false;
-    zrSymId.disabled = false;
-    sym23qsId.disabled = false;
-    sym23qrId.disabled = false;
-    uSymId.disabled = false;
-    rebootId.disabled = false;
-
-    scId.classList.remove("disable");
-    spId.classList.remove("disable");
-    sdId.classList.remove("disable");
-    yId.classList.remove("disable");
-    nId.classList.remove("disable");
-    zsSymId.classList.remove("disable");
-    zmSymId.classList.remove("disable");
-    zrSymId.classList.remove("disable");
-    sym23qsId.classList.remove("disable");
-    sym23qrId.classList.remove("disable");
-    uSymId.classList.remove("disable");
-    rebootId.classList.remove("disable");
+    showAllButtons();
 }
 
 async function sc(){
@@ -245,34 +292,13 @@ async function reboot(){
     try{
         await writeToSym('<sr>');
         textAreaId.innerHTML += '-->> <sr>' + '\n';
+        hhId.disabled = true;
+        hhId.classList.add("disable");
     }catch(error){
         console.log(error);
     }
-    scId.disabled = true;
-    spId.disabled = true;
-    sdId.disabled = true;
-    yId.disabled = true;
-    nId.disabled = true;
-    zsSymId.disabled = true;
-    zmSymId.disabled = true;
-    zrSymId.disabled = true;
-    sym23qsId.disabled = true;
-    sym23qrId.disabled = true;
-    uSymId.disabled = true;
-    rebootId.disabled = true;
-
-    scId.classList.add("disable");
-    spId.classList.add("disable");
-    sdId.classList.add("disable");
-    yId.classList.add("disable");
-    nId.classList.add("disable");
-    zsSymId.classList.add("disable");
-    zmSymId.classList.add("disable");
-    zrSymId.classList.add("disable");
-    sym23qsId.classList.add("disable");
-    sym23qrId.classList.add("disable");
-    uSymId.classList.add("disable");
-    rebootId.classList.add("disable");
+    showMainButtons();
+    hideAllButtons();
 }
 
 function sendMessages(string){
@@ -288,6 +314,88 @@ function sendMessages(string){
 async function delay(ms){
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+// ------------------>>>>>>>
+// ------------------>>>>>>>
+// ------------------>>>>>>>
+
+async function symBluetooth(){
+    document.getElementById("displayInfo").innerHTML = "";
+    const symName = ' SYMU';
+    const options = {
+        filters: [
+                {namePrefix: symName,},
+                {services: ['00001800-0000-1000-8000-00805f9b34fb']},
+                {services: ['0000180a-0000-1000-8000-00805f9b34fb']},
+                {services: ['01234567-89ab-cdef-0123-456789abcdef']},
+                {services: ['49535343-fe7d-4ae5-8fa9-9fafd205e455']}]};
+    try{
+        device = await navigator.bluetooth.requestDevice(options);
+        console.log(device);
+        connect(device);
+    }catch(error){
+        console.log(error);
+    }
+}
+
+async function connect(device){
+    server = await device.gatt.connect();
+    displaySymName(device);
+    if(server.connected){
+        console.log("Is Connected");
+        hideMainButtons();
+        disconnectBluetoothId.style.display = disconnectBluetoothDisplay;
+    }
+    const service = await server.getPrimaryService('49535343-fe7d-4ae5-8fa9-9fafd205e455');
+    getBluetoothService(service);
+}
+
+async function getBluetoothService(service){
+    let str = "hh";
+    const hh = new TextEncoder();
+    const hhDecoder = new TextDecoder();
+    const array = new Int16Array(hh.encode(str));
+    const characteristic = await service.getCharacteristic('49535343-1e4d-4bd9-ba61-23c647249616');
+    await characteristic.writeValue(new Int8Array(new TextEncoder('hh', 'utf-8')));
+    const descriptor = await characteristic.getDescriptor('00002902-0000-1000-8000-00805f9b34fb');
+    console.log(await characteristic.readValue(new DataView(new ArrayBuffer(new Int8Array()))));
+    console.log(await descriptor.readValue(new DataView(new ArrayBuffer(new Int8Array()))));
+    console.log("Done");
+}
+
+function displaySymName(name){
+    document.getElementById("displayInfo").innerHTML += "Name: " + name.name + '<br/>';
+    document.getElementById("displayInfo").innerHTML += "ID: " + name.id + '<br/>'; 
+    document.getElementById("displayInfo").innerHTML += "Server Connected: " + name.gatt + '<br/>';
+}
+
+function disconnectBluetooth(){
+    device.gatt.disconnect();
+    showMainButtons();
+    disconnectBluetoothId.style.display = "none";
+    document.getElementById("displayInfo").innerHTML = "";
+    console.log(server); 
+}
+
+// ------------------>>>>>>>
+// ------------------>>>>>>>
+// ------------------>>>>>>>
+
+async function seeEverything(){
+    try{
+        const allDevices = await navigator.bluetooth.requestDevice({acceptAllDevices: true});
+    }catch(error){
+        console.log(error);
+    }
+}
+
+// navigator.serial.addEventListener("connect", (e) => {
+//     // showButtons();
+// });
+// navigator.serial.addEventListener("disconnect", (e) => {
+//     // hideButtons();
+//     // document.getElementById("purgeAndDisconnect").innerHTML = "";
+// });
 
 
 // async function submitCalibration() {
@@ -338,14 +446,6 @@ async function delay(ms){
 // }
 
 
-// navigator.serial.addEventListener("connect", (e) => {
-//     // showButtons();
-// });
-// navigator.serial.addEventListener("disconnect", (e) => {
-//     // hideButtons();
-//     // document.getElementById("purgeAndDisconnect").innerHTML = "";
-// });
-
 // async function symData(){
 //     try{
 //         await writeToSym('<si>');
@@ -355,75 +455,3 @@ async function delay(ms){
 //         await writeToSym('y');
 //     }catch(error){ console.log(error); }
 // }
-
-
-// async function symByBluetooth(){
-//     document.getElementById("displayErrors").innerHTML = "";
-//     const symName = ' SYMU';
-//     const options = {
-//         filters: [
-//                 {namePrefix: symName,},
-//                 {services: ['00001800-0000-1000-8000-00805f9b34fb']},
-//                 {services: ['0000180a-0000-1000-8000-00805f9b34fb']},
-//                 {services: ['01234567-89ab-cdef-0123-456789abcdef']},
-//                 {services: ['49535343-fe7d-4ae5-8fa9-9fafd205e455']}]};
-//     try{
-//         device = await navigator.bluetooth.requestDevice(options);
-//         console.log(device);
-//         connect(device);
-//     }catch(error){
-//         console.log(error);
-//         document.getElementById("displayErrors").innerHTML = error;
-//     }
-// }
-
-// async function connect(device){
-//     server = await device.gatt.connect();
-//     displaySymName(device);
-//     findButtons.style.display = "none";
-//     disconnectBluetooth.style.display = disconnectBluetoothDisplay;
-//     if(server.connected){
-//         console.log("Is Connected");
-//     }
-//     const service = await server.getPrimaryService('49535343-fe7d-4ae5-8fa9-9fafd205e455');
-//     getBluetoothService(service);
-// }
-
-// async function getBluetoothService(service){
-//     let str = "hh";
-//     const hh = new TextEncoder();
-//     const hhDecoder = new TextDecoder();
-//     const array = new Int16Array(hh.encode(str));
-//     const characteristic = await service.getCharacteristic('49535343-1e4d-4bd9-ba61-23c647249616');
-//     await characteristic.writeValue(new Int8Array(new TextEncoder('hh', 'utf-8')));
-//     const descriptor = await characteristic.getDescriptor('00002902-0000-1000-8000-00805f9b34fb');
-//     console.log(await characteristic.readValue(new DataView(new ArrayBuffer(new Int8Array()))));
-//     console.log(await descriptor.readValue(new DataView(new ArrayBuffer(new Int8Array()))));
-//     console.log("Done");
-// }
-
-// function displaySymName(name){
-//     document.getElementById("name").innerHTML = "Name: " + name.name;
-//     document.getElementById("info").innerHTML = "ID: " + name.id; 
-//     document.getElementById("displayInfo").innerHTML = "Server Connected: " + name.gatt;
-// }
-
-// function disByBluetooth(){
-//     findButtons.style.display = buttonsDisplay;
-//     disconnectBluetooth.style.display = "none";
-//     device.gatt.disconnect();
-//     document.getElementById("name").innerHTML = "";
-//     document.getElementById("info").innerHTML = "";
-//     document.getElementById("displayErrors").innerHTML = "";
-//     document.getElementById("displayInfo").innerHTML = "";
-//     console.log(server); 
-// }
-
-
-async function seeEverything(){
-    try{
-        const allDevices = await navigator.bluetooth.requestDevice({acceptAllDevices: true});
-    }catch(error){
-        console.log(error);
-    }
-}
